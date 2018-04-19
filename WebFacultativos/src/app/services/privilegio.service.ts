fb14c230daf7a807fda81da3bd8b4817f58886e2
Mikel <dopagami@intranet.ibermatica>
2018-04-19 10:21:27 +0200

    prueba

--- WebFacultativos/src/app/services/privilegio.service.ts
+++ WebFacultativos/src/app/services/privilegio.service.ts
@@ -15,6 +15,8 @@ import { Globals } from '../../shared/globals';
 @Injectable()
 export class PrivilegioService {
 
+
+  
   // Url que devuvle un JSON... cambiar por nuestra API
   // private readonly API_URL = 'https://api.github.com/repos/angular/angular/issues';
 
