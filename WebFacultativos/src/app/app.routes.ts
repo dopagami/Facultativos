a425efe23b569ce0513c9aa26182f33ec82b400c
dovemoda <d.velazquez.montana@ibermatica.com>
2018-04-24 15:26:28 +0200

    Keycloak

--- WebFacultativos/src/app/app.routes.ts
+++ WebFacultativos/src/app/app.routes.ts
@@ -1,19 +1,24 @@
 
 import { RouterModule, Routes } from '@angular/router';
-import {HomeComponent} from './components/home/home.component';
-import {PrivilegiosComponent} from './components/privilegios/privilegios.component';
-import {UsertableComponent} from './components/usertable/usertable.component';
+import { HomeComponent } from './components/home/home.component';
+import { PrivilegiosComponent } from './components/privilegios/privilegios.component';
+import { UsertableComponent } from './components/usertable/usertable.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthGuardService } from './services/auth-guard.service';
 
 const APP_ROUTES: Routes = [
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuardService] },
-  { path: 'privilegios', component: PrivilegiosComponent, canActivate: [AuthGuardService] },
-  { path: 'usuarios', component: UsertableComponent, canActivate: [AuthGuardService] },
+  // { path: 'home', component: HomeComponent, canActivate: [AuthGuardService] },
+  // { path: 'privilegios', component: PrivilegiosComponent, canActivate: [AuthGuardService] },
+  // { path: 'usuarios', component: UsertableComponent, canActivate: [AuthGuardService] },
+  // { path: 'login', component: LoginComponent },
+  // { path: '**', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent  },
+  { path: 'privilegios', component: PrivilegiosComponent  },
+  { path: 'usuarios', component: UsertableComponent },
   { path: 'login', component: LoginComponent },
   { path: '**', pathMatch: 'full', redirectTo: 'home' },
 
 ];
 // Forma para poder usar Hashes
-// export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, {useHash:true});
-export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
+ // export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, {useHash:true});
+ export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
